f07aa821fc8878afad00aec15b1aa0cb5a2a38f7
99002548 <71540755+99002548@users.noreply.github.com>
2020-10-10 12:37:34 +0530

    Update cricket.cpp

--- source/cricket.cpp
+++ source/cricket.cpp
@@ -9,7 +9,7 @@
 
 cricket::cricket(): match_id(0),team1(""),team2(""),match_date(""),season_Year(0),venue_Name(""),city_Name(""),country_Name(""),toss_Winner(""),match_winner(""),toss_Name(""),win_Type(""),outcome_Type(""),man_of_the_match(""){}
  
- cricket::cricket(int mid,std::string t1,std::string t2,std::string mdate,int syear,std::string vname,std::string cname,std::string ctryname,std::string twin,std::string m_win,std::string tname,std::string wtype,std::string otype,std::string mom):match_id(mid),team1(t1),team2(t2),match_date(mdate),season_Year(syear),
+ cricket::cricket(int mid,std::string& t1,std::string& t2,std::string& mdate,int syear,std::string& vname,std::string& cname,std::string& ctryname,std::string& twin,std::string& m_win,std::string& tname,std::string& wtype,std::string& otype,std::string& mom):match_id(mid),team1(t1),team2(t2),match_date(mdate),season_Year(syear),
  venue_Name(vname),city_Name(cname),country_Name(ctryname),toss_Winner(twin),match_winner(m_win),
  toss_Name(tname),win_Type(wtype),outcome_Type(otype),man_of_the_match(mom){}
 
